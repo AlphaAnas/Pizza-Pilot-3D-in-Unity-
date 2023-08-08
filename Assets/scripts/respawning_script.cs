fe51fc149a95178e3159f6140ca37a94b8e3e19c
mustafa389 <mustafaasghar66@gmail.com>
2023-08-08 19:18:05 +0500

    added a spawn point(to check the respawning of enemies) and a walking enemy too. both added for testing purposes.

--- Assets/scripts/respawning_script.cs
+++ Assets/scripts/respawning_script.cs
@@ -4,16 +4,15 @@ using UnityEngine;
 
 public class respawning_script : MonoBehaviour
 {
-    // Start is called before the first frame update
     public GameObject Obstacle;
     public float maxX;
     public float minX;
     public float maxY;
     public float minY;
+    public float maxZ; // New variable for max Z-axis value
+    public float minZ; // New variable for min Z-axis value
     public float TimeBetweenSpawn;
     private float SpawnTime;
-
-
 
     // Update is called once per frame
     void Update()
@@ -27,9 +26,12 @@ public class respawning_script : MonoBehaviour
 
     void spawn()
     {
-        float x = Random.Range(minX, maxY);
+        float x = Random.Range(minX, maxX);
         float y = Random.Range(minY, maxY);
+        float z = Random.Range(minZ, maxZ); // Generate random Z-axis value
 
-        Instantiate(Obstacle, transform.position + new Vector3(x, y, 0), transform.rotation);
+        Vector3 spawnPosition = new Vector3(x, y, z);
+
+        Instantiate(Obstacle, spawnPosition, Quaternion.identity);
     }
 }
